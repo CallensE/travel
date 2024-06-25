f634fa90f3a773face487f1006dc306f9bed6ac5
Elien Callens <elienc@infosupport.com>
2024-06-25 13:48:23 +0200

    Added unit test

--- src/app/pipes/temperature.pipe.spec.ts
+++ src/app/pipes/temperature.pipe.spec.ts
@@ -1,8 +1,27 @@
 import { TemperaturePipe } from './temperature.pipe';
 
-describe('TemperaturePipe', () => {
+fdescribe('TemperaturePipe', () => {
+  const pipe = new TemperaturePipe();
+
   it('create an instance', () => {
-    const pipe = new TemperaturePipe();
     expect(pipe).toBeTruthy();
   });
+
+  describe('transform', () => {
+    it('should display temperature in °C when no scale is provided', () => {
+      let result = pipe.transform(24);
+      expect(result).toBe('24 °C');
+    });
+
+    it('should display temperature in °C when Celcius scale is provided', () => {
+      let result = pipe.transform(24, 'Celsius');
+      expect(result).toBe('24 °C');
+    });
+
+    it('should display temperature in °F when Fahrenheit scale is provided', () => {
+      let result = pipe.transform(24, 'Fahrenheit');
+      expect(result).toBe('24 °F');
+    });
+  })
+
 });
