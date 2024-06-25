e162e41cc3b76cc02779d74a956dbc28b6d69d88
Elien Callens <elienc@infosupport.com>
2024-06-25 15:14:20 +0200

    Added template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,33 @@
-import { CommonModule, DatePipe, JsonPipe, UpperCasePipe } from '@angular/common';
+import {
+  CommonModule,
+  DatePipe,
+  JsonPipe,
+  UpperCasePipe,
+} from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TemperaturePipe } from './pipes/temperature.pipe';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Destination } from './models/destination';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, DatePipe, UpperCasePipe, TemperaturePipe],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    DatePipe,
+    UpperCasePipe,
+    TemperaturePipe,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'My travel app';
   imgLocation = '/images/travel.jpg';
-  destinations = [
+  destinations : Destination[] = [
     {
       id: 0,
       country: 'Spain',
@@ -21,7 +35,8 @@ export class AppComponent {
       fromDate: new Date(2024, 8, 3),
       toDate: new Date(2024, 8, 5),
       averageTemperature: 25,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/4/40/M%C3%A1laga_-_vistas_desde_el_Parador_de_Gibralfaro_01_%28cropped%29.jpg'
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/4/40/M%C3%A1laga_-_vistas_desde_el_Parador_de_Gibralfaro_01_%28cropped%29.jpg',
     },
     {
       id: 1,
@@ -30,11 +45,27 @@ export class AppComponent {
       fromDate: new Date(2024, 8, 5),
       toDate: new Date(2024, 8, 8),
       averageTemperature: 26,
-      image: 'https://cdn.getyourguide.com/img/tour/4d9dcddd2b8d52f0.jpeg/70.jpg' 
+      image:
+        'https://cdn.getyourguide.com/img/tour/4d9dcddd2b8d52f0.jpeg/70.jpg',
     },
   ];
 
+  newDestination: Destination = {
+    averageTemperature: 0,
+    city: '',
+    country: '',
+    fromDate: new Date(),
+    toDate: new Date(),
+    id: 0,
+    image: ''
+  };
+
   changeTitle(): void {
     this.title = 'My travel app - changed title';
+  }
+
+  save(destinationForm: NgForm) {
+    this.destinations.push({...this.newDestination});
+    destinationForm.reset();
   }
 }
---
+++ src/app/models/destination.ts
@@ -0,0 +1,9 @@
+export interface Destination {
+    id: number,
+    city: string,
+    country: string, 
+    fromDate: Date,
+    toDate: Date,
+    averageTemperature: number,
+    image: string
+}
